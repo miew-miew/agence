529944dbb03487f8904aa455ba3a3324d9edaa55
miew-miew <nrzanakoto011@gmail.com>
2024-02-11 20:52:04 +0100

    agence

---
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Property;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PropertyFormRequest;
+
+class PropertyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('admin.properties.index', [
+            'properties' => Property::orderBy('created_at', 'desc')->paginate(25)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $property = new Property();
+        $property->fill([
+            'surface' => 40,
+            'rooms' => 3,
+            'bedrooms' => 1,
+            'floor' => 0,
+            'city' => 'Antananarivo',
+            'postal_code' => 101,
+            'sold' => false
+        ]);
+        return view('admin.properties.form', [
+            'property' => $property
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PropertyFormRequest $request)
+    {
+        $property = Property::create($request->validated());
+        return redirect()->route('admin.property.index')->with('success', 'Le bien a bien été créé');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
