e0e8f4dae9d44477d6c68382210ae8ec46807db2
miew-miew <nrzanakoto011@gmail.com>
2024-02-23 17:57:18 +0100

    authentification

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function doLogin(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended(route('admin.property.index'));
+        }
+        return back()->withErrors([
+            'email' => 'Identifiants incorrect'
+        ])->onlyInput('email');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return to_route('login')->with('success', 'Vous avez bien été déconnecté');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\OptionController;
 use App\Http\Controllers\Admin\PropertyController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PropertyController as ControllersPropertyController;
 
@@ -28,8 +29,15 @@ Route::get('/biens/{slug}-{property}', [ControllersPropertyController::class, 's
 Route::post('/biens/{property}/contact', [ControllersPropertyController::class, 'contact'])->name('property.contact')->where([
     'property' => $idRegex
 ]);
+Route::get('/login', [AuthController::class, 'login'])
+->middleware('guest')
+->name('login');
+Route::post('/login', [AuthController::class, 'doLogin']);
+Route::delete('/logout', [AuthController::class, 'logout'])
+->middleware('auth')
+->name('logout');
 
-Route::prefix('admin')->name('admin.')->group(function(){
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function(){
     Route::resource('property', PropertyController::class)->except(['show']);
     Route::resource('option', OptionController::class)->except(['show']);
 });
