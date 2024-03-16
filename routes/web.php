2d8767c06369c50883d2175423bb4c6565bab3a2
miew-miew <nrzanakoto011@gmail.com>
2024-03-16 18:13:08 +0100

    authentification starter-kits

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Admin\PropertyController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PropertyController as ControllersPropertyController;
-
+use App\Http\Controllers\ProfileController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,15 +29,20 @@ Route::get('/biens/{slug}-{property}', [ControllersPropertyController::class, 's
 Route::post('/biens/{property}/contact', [ControllersPropertyController::class, 'contact'])->name('property.contact')->where([
     'property' => $idRegex
 ]);
-Route::get('/login', [AuthController::class, 'login'])
-->middleware('guest')
-->name('login');
-Route::post('/login', [AuthController::class, 'doLogin']);
-Route::delete('/logout', [AuthController::class, 'logout'])
-->middleware('auth')
-->name('logout');
 
-Route::prefix('admin')->name('admin.')->middleware('auth')->group(function(){
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified'])->group(function(){
     Route::resource('property', PropertyController::class)->except(['show']);
     Route::resource('option', OptionController::class)->except(['show']);
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
